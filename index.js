ade4f8a1bddbcbe5d008852fffd0ea2e8281b76e
Agustin Lendaro <agustinlendaro@hotmail.com>
2022-08-02 20:16:24 -0300

    tercer commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function nuevaImpresion (){
         tamañoIngresado = prompt ("Elija un número entre 1 y 20");
         }
         //complejidad
-        let preguntaComplejidad = prompt ("Elija alta, mediana, baja").toLowerCase()
+        let preguntaComplejidad = prompt ("Elija nivel de complejidad alta, mediana, baja").toLowerCase()
          convertirComplejidad()
 
         function preguntarComplejidadDeNuevo (){ while (preguntaComplejidad != "alta" && preguntaComplejidad != "mediana" && preguntaComplejidad != "baja"){
@@ -50,14 +50,14 @@ function nuevaImpresion (){
 
 
 function menu(){
-    let eleccionMenu = parseInt(prompt ("Elija una opcion: 1- Agregar impresión 2-Ver listado (en consola) 3- Salir"))
+    let eleccionMenu = parseInt(prompt ("Elija una opcion: 1- Agregar impresión 2- Impresiones en el carrito 3- Salir"))
     if (eleccionMenu <= 3 && eleccionMenu >= 1)  
     switch (eleccionMenu){
     case 1: nuevaImpresion(); menu()
     break;
-    case 2: listaFinal(); menu()
+    case 2: listaFinal();
     break;
-    case 3: alert("Gracias por venir")
+    case 3: alert("¡Gracias por venir, vuelva pronto!")
     break;
     default: alert ("Error de switch")
     }
@@ -66,58 +66,12 @@ function menu(){
 }
 
 function listaFinal(){
-    productos.forEach((producto) => console.log(`Su producto ${producto.nombre} tiene un valor de $${producto.tamaño * precioBase * producto.complejidad} y un tiempo aproximado de impresión de ${parseInt(producto.tamaño * producto.complejidad / factorVelocidad)} dias.`))
+    if (productos.length == 0)
+    alert ("Su carrito está vacío, agregue algo eligiendo 1.")
+    else 
+    productos.forEach((producto) => console.log(`Su producto ${producto.nombre} tiene un valor de $${producto.tamaño * precioBase * producto.complejidad} y un tiempo aproximado de impresión de ${parseInt(producto.tamaño * producto.complejidad / factorVelocidad)} dias.`));
+    menu()
 }
 
 menu()
 
-
-
-/*
-let complejidad
-let prioridad
-let factorVelocidad = 3
-let precioBase = 250
-
-let preguntaTamaño = prompt ("Elija un tamaño en cm entre 1 y 20 de alto para la impresión 3d")
-
-while (preguntaTamaño>20 || (isNaN(preguntaTamaño)) ){
-    preguntaTamaño = prompt ("Elija un número entre 1 y 20")
-    console.log(preguntaTamaño);
-}
-
-
-
-let preguntaComplejidad = prompt ("Elija alta, mediana, baja").toLowerCase()
-convertirTamaño()
-
-function preguntarComplejidadDeNuevo (){ while (preguntaComplejidad != "alta" && preguntaComplejidad != "mediana" && preguntaComplejidad != "baja"){
-    preguntaComplejidad = prompt ("Elija un valor válido: alta, mediana, baja").toLowerCase()
-    convertirTamaño()
-    }
-}
-
-function convertirTamaño(){
-switch (preguntaComplejidad){
-    case "alta":  complejidad = 1.50
-    break;
-    case "mediana":  complejidad = 1.25
-    break;
-    case "baja": complejidad = 1
-    break;
-    default: preguntarComplejidadDeNuevo()
-}
-}
-
-
-function tiempo (){
-    return parseInt(preguntaTamaño * complejidad / factorVelocidad)
-}
-
-function precio(){
-    return precioBase * preguntaTamaño * complejidad
-}
-
-
-alert (`Su pedido tiene un costo de $${precio()} y una demora de aproximada de ${tiempo()} días.`)
-*/
